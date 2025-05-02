2d66d82c72f7546487fb2130d820271dabfb4d1e
Fanneen SA <salahmap9909@gmail.com>
2025-05-02 16:50:45 +0200

    Initial clean commit

---
+++ env.js
@@ -0,0 +1,13 @@
+// env.js
+window._env_ = {
+  FIREBASE_API_KEY:        "AIzaSyAQ2FQL0DvbPx4YnJrrAmal2o-yaH_KP_s",
+  FIREBASE_AUTH_DOMAIN:    "adlo-44d1b.firebaseapp.com",
+  FIREBASE_DATABASE_URL:   "https://adlo-44d1b-default-rtdb.firebaseio.com",
+  FIREBASE_PROJECT_ID:     "adlo-44d1b",
+  FIREBASE_STORAGE_BUCKET: "adlo-44d1b.appspot.com",
+  FIREBASE_MESSAGING_SENDER_ID: "96260637269",
+  FIREBASE_APP_ID:         "1:96260637269:web:e3bc37ab2b41038c254adc",
+  FIREBASE_MEASUREMENT_ID: "G-R0SGBWC3CJ",
+
+  MAPBOX_ACCESS_TOKEN:     "pk.eyJ1IjoiY2hvb3NlbWUiLCJhIjoiY20wZnVpemI2MHpreTJqcjR4ZThuam9pYyJ9.F1CjHrnJ7m-VwM_LHt0Q9Q"
+};
